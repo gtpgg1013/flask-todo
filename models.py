1fc6b6d6fc5df508a501bc37574a2e0711c624ad
Taehyung Kang <gtpgg1013@gmail.com>
2019-02-27 07:51:45 +0000

    first commit

--- models.py
+++ models.py
@@ -7,7 +7,17 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     deadline = db.Column(db.DateTime)
+    comments = db.relationship("Comment")
     
     def __init__(self,title,deadline):
         self.title = title
         self.deadline = deadline
+        
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=True)
+    todo_id = db.Column(db.Integer, db.ForeignKey('todos.id'), nullable=True)
+    
+    def __init__(self, content):
+        self.content = content
